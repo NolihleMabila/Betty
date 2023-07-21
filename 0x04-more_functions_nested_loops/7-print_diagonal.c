95af006c508722b63b05bb92b6e3095e4e3fdd00
Your Name <you@example.com>
2023-07-21 13:55:38 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,20 +10,17 @@ void print_diagonal(int n)
 	int i;
 	int j;
 
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (i = 1; i <= n; i++)
-		{
-			for (j = 1; j <= i; j++)
-			{
-				_putchar(' ');
-			}
-			_putchar('\\');
-			_putchar('\n');
-		}
+		_putchar('\n');
 	}
-	else
+	for (i = 1; i <= n; i++)
 	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
 		_putchar('\n');
 	}
 }
