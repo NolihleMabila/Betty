840f0a8c067b86b52b55360fefa5e09321591449
Your Name <you@example.com>
2023-07-20 18:50:04 -0700

    functions and loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * _isupper - check for uppercase
+ *
+ * @c: check for character
+ *Return: 1 for upercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if c >= 'A' && c <= 'Z'
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
