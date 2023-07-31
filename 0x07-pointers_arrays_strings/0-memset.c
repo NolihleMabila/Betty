e686c26f69b7fef1f2a00fb0234f6d87fc15c26a
Your Name <you@example.com>
2023-07-31 15:05:23 -0700

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset -  function fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * @s: starting address 
+ * @b: the output value
+ * @n: bytes to be changed
+ * Return: new array of  n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
