09dd89b85887334d33ad190f8589adf1e96abd63
Your Name <you@example.com>
2023-08-03 09:07:29 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - is a function that prints the name of the file
+ * @argc: input char
+ * @argv: input  char
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
